cca72f7b6936c08f1123ca865bc369e21d91349e
SergioCancinos <cancinos.sergio@gmail.com>
2014-11-10 14:11:59 -0600

    new commit

---
+++ src/lab9/WriteFile.java
@@ -0,0 +1,34 @@
+/*
+*	Sergio Cancinos 13062
+*	Angel Basegoda	 13256
+*	WriteFile.java	
+*	
+*/
+
+package Lab9;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class WriteFile{
+	private String path;
+	private boolean append_to_file = false;
+	
+	public WriteFile (String file_path){
+		path = file_path;
+	}
+
+	public WriteFile(String file_path, boolean append_value){
+		path = file_path;
+		append_to_file = append_value;
+	}
+	public void writeToFile (String textLine) throws IOException{
+	
+	FileWriter write = new FileWriter (path, append_to_file);
+	PrintWriter print_line = new PrintWriter(write);
+	print_line.printf("%s" + "%n", textLine);
+	print_line.close();
+	
+	}
+
+}
